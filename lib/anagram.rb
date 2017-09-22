aa5ef8c80984617371eb9b0074c6cdd99b77130b
rebeccanewborn <rjn58@cornell.edu>
2017-09-22 13:44:54 -0400

    finished

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :sorted_word_array
+
+  def initialize(word)
+    @sorted_word_array = word.split("").sort
+  end
+
+  def match(array)
+    array.select do |word|
+      word.split("").sort == sorted_word_array
+    end
+  end
+
+end
